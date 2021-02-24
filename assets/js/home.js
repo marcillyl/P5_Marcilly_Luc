cd9640d14c3930617fbae2481e38fa81d69321f5
marcillyl <marcillyluc@icloud.com>
2021-02-24 11:03:32 +0100

    sass update

--- assets/js/home.js
+++ assets/js/home.js
@@ -7,16 +7,13 @@ function displayProductList (array) {
         const cardShade = document.createElement("div");
         const productName = document.createElement("h3");
         const productImg = document.createElement("img");
-        const icon = document.createElement("i");
         productList.appendChild(productCard);
         productCard.appendChild(cardShade);
         cardShade.appendChild(productImg);
         productCard.appendChild(productName);
-        productCard.appendChild(icon);
-        icon.className = "fas fa-search";
         cardShade.className = "shade";
         productCard.href = `product.html?productId=${elem._id}`;
-        productName.innerHTML = elem.name;
+        productName.innerHTML = `<span class="alt">Product_</span> ${elem.name}`
         productImg.src = elem.imageUrl;
     }
 }
