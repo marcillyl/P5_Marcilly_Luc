0b27ad6d3f9e235e9883588e2440a59b6b43a2a2
marcillyl <marcillyluc@icloud.com>
2021-03-17 14:19:13 +0100

    update (html / add order files)

---
+++ assets/js/order.js
@@ -0,0 +1,27 @@
+const vCam = ['http://localhost:3000/api/cameras']
+
+function displayProductList (array) {
+    for (let elem of array) {
+        let productList = document.getElementById("productList");
+        const productCard = document.createElement("a");
+        const cardShade = document.createElement("div");
+        const productName = document.createElement("h3");
+        const productImg = document.createElement("img");
+        productList.appendChild(productCard);
+        productCard.appendChild(cardShade);
+        cardShade.appendChild(productImg);
+        productCard.appendChild(productName);
+        cardShade.className = "shade";
+        productCard.href = `product.html?productId=${elem._id}`;
+        productName.innerHTML = `<span class="alt">Product_</span> ${elem.name}`
+        productImg.src = elem.imageUrl;
+    }
+}
+
+async function main () {
+    const cameras = await fetch(vCam)
+        .then((response) => response.json());
+    displayProductList(cameras);
+};
+
+main();
